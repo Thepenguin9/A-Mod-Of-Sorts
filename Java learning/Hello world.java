d6e0b0c56c71f78fff1cb0dd1b1878f8f0443f9c
Thepenguin9 <github@thepenguin9.co.uk>
2014-05-26 16:13:16 +0000

    Learning Java!

---
+++ Java learning/Hello world.java
@@ -0,0 +1,9 @@
+/**
+* HelloWorldApp class implements application that prints Hello World!"
+* to standard output
+*/
+class HelloWorldApp {
+	public static void main(String[] args)	{
+		System.out.printIn("Hello World!"); //Displays text (string)
+	}
+}

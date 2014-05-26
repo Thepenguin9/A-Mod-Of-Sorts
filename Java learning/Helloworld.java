d0f77025746a6f721ede13a11a09376f1084c0ae
Thepenguin9 <github@thepenguin9.co.uk>
2014-05-26 16:23:17 +0000

    yay

--- Java learning/Helloworld.java
+++ Java learning/Helloworld.java
@@ -3,7 +3,7 @@
 * to standard output
 */
 class HelloWorldApp {
-	public static void main(String[] args)	{
-		System.out.printIn("Hello World!"); //Displays text (string)
+	public static void main(String args[]){
+		System.out.println("Hello World!"); //Displays text print is Ln (string)
 	}
 }
